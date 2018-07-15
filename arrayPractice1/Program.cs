6cb66a6e5e6fe79dee9daebdb177b8f703a5ee37
Robin Rolfe <41052323+RobinRolfe@users.noreply.github.com>
2018-07-15 21:23:54 +0100

    Update Program.cs
    Added comments

--- arrayPractice1/Program.cs
+++ arrayPractice1/Program.cs
@@ -7,19 +7,19 @@ namespace arrayPractice1
         static void Main(string[] args)
         {
 
-            string[] valueOne = new string[5];
+            string[] valueOne = new string[5]; //declare and initialise the array
             
-
-            Console.WriteLine("please enter a number");
+        
+            Console.WriteLine("please enter a number");//promt user for input
 
             for (int i = 0; i < valueOne.Length; i++)
             {
-                valueOne[i] = Console.ReadLine();
+                valueOne[i] = Console.ReadLine();//assign value
 
                 Console.WriteLine("please enter another number");
             }
 
-            for (int i = 0; i < valueOne.Length; i++)
+            for (int i = 0; i < valueOne.Length; i++)//print the values to the screen
             {
                 Console.WriteLine(valueOne[i]);
             }
