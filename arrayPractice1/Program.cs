295f0da8ff611f588e13ffa489c8f736bd261a65
ROBIN ALEXANDER ROLFE <S14001664@mail.glyndwr.ac.uk>
2018-07-15 21:09:28 +0100

    Add project files.

---
+++ arrayPractice1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace arrayPractice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] valueOne = new string[5];
+            
+
+            Console.WriteLine("please enter a number");
+
+            for (int i = 0; i < valueOne.Length; i++)
+            {
+                valueOne[i] = Console.ReadLine();
+
+                Console.WriteLine("please enter another number");
+            }
+
+            for (int i = 0; i < valueOne.Length; i++)
+            {
+                Console.WriteLine(valueOne[i]);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
